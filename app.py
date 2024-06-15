c984bbff1b8bab15de6bb53408fec42018fd0eef
Misty <aluadotiago@gmail.com>
2024-06-15 18:10:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,60 +1,143 @@
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output
 
 df_dxy = pd.read_csv('dxy_daily_2023_marketwatch.csv')
-df_gold = pd.read_csv('gold_daily_2023_metaquotesdemo.csv')
+df_xau = pd.read_csv('xauusd_daily_2023_metaquotesdemo.csv')
 df_eur = pd.read_csv('eurusd_daily_2023_metaquotesdemo.csv')
+df_gbp = pd.read_csv('gbpusd_daily_2023_metaquotesdemo.csv')
+df_aud = pd.read_csv('audusd_daily_2023_metaquotesdemo.csv')
 
 # armazenar 'data e fechamento diário' num dataframe independente
 df_dxy_close = df_dxy[['date', 'close']].copy()
-df_gold_close = df_gold[['date', 'close']].copy()
+df_xau_close = df_xau[['date', 'close']].copy()
 df_eur_close = df_eur[['date', 'close']].copy()
+df_gbp_close = df_gbp[['date', 'close']].copy()
+df_aud_close = df_aud[['date', 'close']].copy()
 
 # converter valores na coluna data em tipo datetime de ambos dataframes
-df_gold_close['date'] = pd.to_datetime(df_gold_close['date'])
+df_xau_close['date'] = pd.to_datetime(df_xau_close['date'])
 df_dxy_close['date'] = pd.to_datetime(df_dxy_close['date'])
 df_eur_close['date'] = pd.to_datetime(df_eur_close['date'])
+df_gbp_close['date'] = pd.to_datetime(df_gbp_close['date'])
+df_aud_close['date'] = pd.to_datetime(df_aud_close['date'])
 
 # ordenar valores no dataframe do índice do dólar
 df_dxy_close = df_dxy_close.sort_values(by='date')
 
-# juntar dataframes em um só
-dxy_gold = df_dxy_close.merge(df_gold_close, on='date')
-dxy_gold_eur = dxy_gold.merge(df_eur_close, on='date')
-
-# cria e personaliza os gráficos
-fig_dxy = px.line(df_dxy_close, x="date", y="close")
-fig_gold = px.line(df_gold_close, x="date", y="close")
-fig_junto = go.Figure()
-fig_junto.add_trace(go.Scatter(x=df_dxy_close['date'], y=df_dxy_close['close'], mode='lines', name='Índice do Dólar', yaxis='y1'))
-fig_junto.add_trace(go.Scatter(x=df_gold_close['date'], y=df_gold_close['close'], mode='lines', name='Histórico XAUUSD', yaxis='y2'))
-fig_junto.update_layout(
+# figura DXY x XAUUSD
+fig_xau = go.Figure()
+fig_xau.add_trace(go.Scatter(x=df_dxy_close['date'], y=df_dxy_close['close'], mode='lines', name='Índice do Dólar', yaxis='y1'))
+fig_xau.add_trace(go.Scatter(x=df_xau_close['date'], y=df_xau_close['close'], mode='lines', name='Histórico XAUUSD', yaxis='y2'))
+fig_xau.update_layout(
     xaxis_title='Data',
-    yaxis=dict(title='ÍNDICE DO DÓLAR', titlefont=dict(color='#1f77b4'), tickfont=dict(color='#1f77b4')),
-    yaxis2=dict(title='HISTÓRICO XAUUSD', titlefont=dict(color='#ff7f0e'), tickfont=dict(color='#ff7f0e'), overlaying='y', side='right'),
-    legend=dict(x=0.1, y=0.9)
+    yaxis=dict(
+        title='ÍNDICE DO DÓLAR',
+        titlefont=dict(color='#1f77b4'),
+        tickfont=dict(color='#1f77b4')),
+    yaxis2=dict(
+        title='HISTÓRICO XAUUSD',
+        titlefont=dict(color='#ff7f0e'),
+        tickfont=dict(color='#ff7f0e'),
+                overlaying='y',
+                side='right'
+                ),
+    legend=dict(x=0.2, y=0.8)
 )
-# configura app dash
+
+# figura DXY x EURUSD
+fig_eur = go.Figure()
+fig_eur.add_trace(go.Scatter(x=df_dxy_close['date'], y=df_dxy_close['close'], mode='lines', name='Índice do Dólar', yaxis='y1'))
+fig_eur.add_trace(go.Scatter(x=df_eur_close['date'], y=df_eur_close['close'], mode='lines', name='Histórico EURUSD', yaxis='y2'))
+fig_eur.update_layout(
+    xaxis_title='Data',
+    yaxis=dict(
+        title='ÍNDICE DO DÓLAR',
+        titlefont=dict(color='#1f77b4'),
+        tickfont=dict(color='#1f77b4')),
+    yaxis2=dict(
+        title='HISTÓRICO EURUSD',
+        titlefont=dict(color='#ff7f0e'),
+        tickfont=dict(color='#ff7f0e'),
+                overlaying='y',
+                side='right'
+                ),
+    legend=dict(x=0.2, y=0.8)
+)
+
+# figura DXY x GBPUSD
+fig_gbp = go.Figure()
+fig_gbp.add_trace(go.Scatter(x=df_dxy_close['date'], y=df_dxy_close['close'], mode='lines', name='Índice do Dólar', yaxis='y1'))
+fig_gbp.add_trace(go.Scatter(x=df_gbp_close['date'], y=df_gbp_close['close'], mode='lines', name='Histórico GBPUSD', yaxis='y2'))
+fig_gbp.update_layout(
+    xaxis_title='Data',
+    yaxis=dict(
+        title='ÍNDICE DO DÓLAR',
+        titlefont=dict(color='#1f77b4'),
+        tickfont=dict(color='#1f77b4')),
+    yaxis2=dict(
+        title='HISTÓRICO GBPUSD',
+        titlefont=dict(color='#ff7f0e'),
+        tickfont=dict(color='#ff7f0e'),
+                overlaying='y',
+                side='right'
+                ),
+    legend=dict(x=0.2, y=0.8)
+)
+
+# figura DXY x AUDUSD
+fig_aud = go.Figure()
+fig_aud.add_trace(go.Scatter(x=df_dxy_close['date'], y=df_dxy_close['close'], mode='lines', name='Índice do Dólar', yaxis='y1'))
+fig_aud.add_trace(go.Scatter(x=df_aud_close['date'], y=df_aud_close['close'], mode='lines', name='Histórico AUDUSD', yaxis='y2'))
+fig_aud.update_layout(
+    xaxis_title='Data',
+    yaxis=dict(
+        title='ÍNDICE DO DÓLAR',
+        titlefont=dict(color='#1f77b4'),
+        tickfont=dict(color='#1f77b4')),
+    yaxis2=dict(
+        title='HISTÓRICO AUDUSD',
+        titlefont=dict(color='#ff7f0e'),
+        tickfont=dict(color='#ff7f0e'),
+                overlaying='y',
+                side='right'
+                ),
+    legend=dict(x=0.2, y=0.8)
+)
+
 app = Dash(__name__)
+# layout da aplicação
 app.layout = html.Div([ # componente principal
     html.H1(children='INSIGHT DE TENDÊNCIA', style={'text-align': 'center'}),
-    
-    html.Div([ # componente dxy
-        html.H1(children='ÍNDICE DO DÓLAR', style={'text-align': 'center'}),
-        dcc.Graph(figure=fig_dxy)],
-        style={'width':'50%', 'display':'inline-block'}),
 
-    html.Div([ # componente gold
-        html.H1(children='HISTÓRICO XAUUSD', style={'text-align': 'center'}),
-        dcc.Graph(figure=fig_gold)],
-        style={'width':'50%','display':'inline-block'}),
-    
-    html.Div([ # componente inferência
-        html.H1(children='INFERÊNCIA', style={'text-align': 'center'}),
-        dcc.Graph(figure=fig_junto)],
-        style={'width':'100%','display':'block'})
+    # gráfico inferência
+    html.Div([
+        # botão de selecionar relação   
+        dcc.Dropdown(['XAU', 'EUR', 'GBP', 'AUD'], value='XAU', id='relacao_dxy',
+                     style={'width':'100%','text-align': 'center', 'display': 'inline-block'}),
+        dcc.Graph(id='inferencia', figure=fig_xau)],
+        style={'display': '100%', 'justify-content': 'center', 'align-items': 'center'})
 ])
+
+# callbacks da aplicação
+@app.callback(
+    Output('inferencia', 'figure'),
+    Input('relacao_dxy', 'value')
+)
+def update_output(value):
+    if value == 'XAU':
+        # figura do xau
+        return fig_xau
+    elif value == 'EUR':
+        # figura do eur
+        return fig_eur
+    elif value == 'GBP':
+        # figura do GBP
+        return fig_gbp
+    elif value == 'AUD':
+        # figura do AUD
+        return fig_aud
+
 if __name__ == '__main__':
     app.run_server(host='0.0.0.0', port=8050)
